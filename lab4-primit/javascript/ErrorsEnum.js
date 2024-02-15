db5031354c32c24b1a74f9fdd537d2dc3b687f49
Eddie Poulter <eddie.poulter@outlook.com>
2024-02-15 11:35:16 +0000

    Added lab 4

--- lab4-primit/javascript/ErrorsEnum.js
+++ lab4-primit/javascript/ErrorsEnum.js
@@ -16,34 +16,52 @@ const Result_enumobj = {
 
 function error2Result(err){
     switch (err) {
-	case Error_enumobj.FP_ROUNDING:
-	 return Result_enumobj.A_BIT_DIFFERENT;
-	break;
-	case Error_enumobj.FP_OVERFLOW:
-	    return Result_enumobj.INFINITY;
-	break;
-	case Error_enumobj.FP_UNDERFLOW:
-	    return Result_enumobj.ZERO;
-	break;
-	case Error_enumobj.INT_OVERFLOW:
-	    return Result_enumobj.VERY_DIFFERENT;
-	break;
-	default:
-		return 'Invalid Error value';
+        case Error_enumobj.FP_ROUNDING:
+            return Result_enumobj.A_BIT_DIFFERENT;
+            break;
+        case Error_enumobj.FP_OVERFLOW:
+            return Result_enumobj.INFINITY;
+            break;
+        case Error_enumobj.FP_UNDERFLOW:
+            return Result_enumobj.ZERO;
+            break;
+        case Error_enumobj.INT_OVERFLOW:
+            return Result_enumobj.VERY_DIFFERENT;
+            break;
+        default:
+            return 'Invalid Error value';
+    }
 }
 
+function result2Error(res) {
+    switch (res) {
+        case Result_enumobj.A_BIT_DIFFERENT:
+            return Error_enumobj.FP_ROUNDING;
+            break;
+        case Result_enumobj.INFINITY:
+            return Error_enumobj.FP_OVERFLOW;
+            break;
+        case Result_enumobj.ZERO:
+            return Error_enumobj.FP_UNDERFLOW;
+            break;
+        case Result_enumobj.VERY_DIFFERENT:
+            return Error_enumobj.INT_OVERFLOW;
+            break;
+        default:
+            return 'Invalid Result value';
+    }
 }
 
-console.log('Error list: ', Object.values(Error_enumobj));
+console.log('Result list: ', Object.values(Result_enumobj));
 var validArg = false;
 while(!validArg){
     var input = prompt("Input: ");
-    let result = error2Result(input);
-    if (Object.values(Result_enumobj).includes(result)){
+    let error = result2Error(input);
+    if (Object.values(Error_enumobj).includes(error)){
         validArg = true;
-		console.log(input + " results in " + error2Result(input));
+		console.log(input + " results in " + result2Error(input));
     }
     else{
-        console.log(result);
+        console.log(error);
     }
 }
